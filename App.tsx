c90727e1b6628451a67577d69cb794d689cf8c4c
yoovin <soo22839@gmail.com>
2023-03-25 00:58:46 +0900

    첫 세팅

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+function App(): JSX.Element {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+
+export default App;
